6e71e37ecb1914c2ce7cb120d37f6b6862372b82
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-08 23:03:20 +0300

    added sign-up & sign-in apis

--- instagram-BE/app/Http/Controllers/API/AuthController.php
+++ instagram-BE/app/Http/Controllers/API/AuthController.php
@@ -43,30 +43,32 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6',
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'username' => 'required|string|unique:users',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+            $user = User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
 
-        return response()->json([
-            'message' => 'User created successfully',
-            'user' => $user
-        ]);
-    }
+            return response()->json([
+                'message' => 'User created successfully',
+                'user' => $user
+            ], 201);
+        } catch (\Exception $e) {
 
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json([
-            'message' => 'Successfully logged out',
-        ]);
+            return response()->json([
+                'message' => 'Error during registration',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function refresh()
