cdceb126e5cde4428c370d68e71e88f7b0ac2d83
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-09 13:25:18 +0300

    cleaned the files to start

--- contact-manager-fe/src/App.js
+++ contact-manager-fe/src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
 import './App.css';
-import Contacts from "./pages/Contacts"
 import Signin from "./pages/Signin"
-import AddContact from "./pages/AddContact"
-import ContactDetails from "./pages/ContactDetails"
+
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
         <Route path="/contacts" >
           <Route index element={<Contacts/>} />
         </Route>
-        <Route path='/contacts/add' element={<AddContact/>} /> 
-         <Route path='/ContactDetails/:id' element={<ContactDetails/>} /> 
         <Route path='*' element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
