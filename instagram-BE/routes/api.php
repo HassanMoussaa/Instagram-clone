03db5f15fa6cb92b134e835fb05d9358a2d54994
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-09 13:16:08 +0300

    added authentication to my apis

--- instagram-BE/routes/api.php
+++ instagram-BE/routes/api.php
@@ -19,30 +19,34 @@ use App\Http\Controllers\PostController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+
+Route::middleware('auth:api')->group(function () {
+
+
+
+
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    });
+
+
+
+    Route::get('posts/following', [PostController::class, 'getFollowingPosts']);
+    Route::post('user/follow/{userId}', [FollowController::class, 'followUser']);
+    Route::post('user/unfollow/{userId}', [FollowController::class, 'unfollowUser']);
+
+
+    Route::post('posts/add', [PostController::class, 'createPost']);
+    Route::post('posts/like/{postId}', [PostController::class, 'likePost']);
+    Route::post('posts/unlike/{postId}', [PostController::class, 'unlikePost']);
+    Route::get('/posts/liked{postId}', [PostController::class, 'isPostLikedByUser']);
+
+    Route::get('/search/users', [SearchController::class, 'searchUsers']);
+
+
+
+
 });
-
-
-
-Route::get('posts/following', [PostController::class, 'getFollowingPosts']);
-Route::post('user/follow/{userId}', [FollowController::class, 'followUser']);
-Route::post('user/unfollow/{userId}', [FollowController::class, 'unfollowUser']);
-
-
-Route::post('posts/add', [PostController::class, 'createPost']);
-Route::post('posts/like/{postId}', [PostController::class, 'likePost']);
-Route::post('posts/unlike/{postId}', [PostController::class, 'unlikePost']);
-Route::get('/posts/liked{postId}', [PostController::class, 'isPostLikedByUser']);
-
-Route::get('/search/users', [SearchController::class, 'searchUsers']);
-
-
-// Route::middleware('auth:api')->group(function () {
-
-
-
-// });
