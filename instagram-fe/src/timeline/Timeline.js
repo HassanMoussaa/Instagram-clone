75f018c1a1354a0578ff0746f8d30297f5e0c66d
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-09 21:26:15 +0300

    added timeline posts (dummuy) and stylied them

--- instagram-fe/src/timeline/Timeline.js
+++ instagram-fe/src/timeline/Timeline.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./Timeline.css"
 import Sugesstions from './Sugesstions'
+import Post from './posts/Post'
 
 function Timeline() {
   return (
     <div className='timeline'>
-     <div className='timeline__left'>Timeline</div>
+     <div className='timeline__left'>
+     <Post />   
+     <Post /> 
+      <Post /> 
+     </div>
      <div className='timeline__right'>
         <Sugesstions />
      </div>
