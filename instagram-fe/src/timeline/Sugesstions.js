5ec5523cd5a48834a2f7cdf5d2e3de737b545b32
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-10 02:45:56 +0300

    cleaed the suggestions section

--- instagram-fe/src/timeline/Sugesstions.js
+++ instagram-fe/src/timeline/Sugesstions.js
@@ -19,45 +19,6 @@ function Suggestions() {
           </div>
           <button className="follow__button">Follow</button>
         </div>
-
-        <div className="suggestions__username">
-          <div className="username__left">
-            <span className="avatar">
-              <Avatar>R</Avatar>
-            </span>
-            <div className="username__info">
-              <span className="username">redian_</span>
-              <span className="relation">New to Instagram</span>
-            </div>
-          </div>
-          <button className="follow__button">Follow</button>
-        </div>
-
-        <div className="suggestions__username">
-          <div className="username__left">
-            <span className="avatar">
-              <Avatar>R</Avatar>
-            </span>
-            <div className="username__info">
-              <span className="username">redian_</span>
-              <span className="relation">New to Instagram</span>
-            </div>
-          </div>
-          <button className="follow__button">Follow</button>
-        </div>
-
-        <div className="suggestions__username">
-          <div className="username__left">
-            <span className="avatar">
-              <Avatar>R</Avatar>
-            </span>
-            <div className="username__info">
-              <span className="username">redian_</span>
-              <span className="relation">New to Instagram</span>
-            </div>
-          </div>
-          <button className="follow__button">Follow</button>
-        </div>
       </div>
     </div>
   );
