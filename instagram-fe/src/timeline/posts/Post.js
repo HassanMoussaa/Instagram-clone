12e5754e06e8cfe145565e41f81e290e26255788
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-10 11:05:58 +0300

    added like feature

--- instagram-fe/src/timeline/posts/Post.js
+++ instagram-fe/src/timeline/posts/Post.js
@@ -1,14 +1,68 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import "./Post.css";
+import axios from 'axios'; 
+import Cookies from "js-cookie";
 
+function Post({ post }) {
+  const [isLiked, setIsLiked] = useState(!post.is_liked);  
+  const jwtToken = Cookies.get("jwt_token");
 
-function post({ post }) {
+  useEffect(() => {
+    checkIsLiked();
+  }, []);
+
+  const checkIsLiked = async () => {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/api/posts/liked/${post.id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${jwtToken}`,
+          },
+        }
+      );
+      setIsLiked(response.data.liked);
+    } catch (error) {
+      console.error("Error checking if post is liked:", error);
+    }
+  };
+
+  const handleLikeClick = async () => {
+    try {
+      if (isLiked) {
+        
+        await axios.post(
+          `http://127.0.0.1:8000/api/posts/unlike/${post.id}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          }
+        );
+      } else {
+       
+        await axios.post(
+          `http://127.0.0.1:8000/api/posts/like/${post.id}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          }
+        );
+      }
+      setIsLiked(!isLiked);  
+    } catch (error) {
+      console.error("Error liking/unliking post:", error);
+    }
+  };
   return (
     <div className='post'>
       <div className='post__header'>
@@ -24,7 +78,11 @@ function post({ post }) {
       <div className="post__footer">
         <div className="post__footerIcons">
           <div className="post__iconsMain">
-            <FavoriteBorderIcon className="postIcon" />
+              {isLiked ? (
+                  <FavoriteBorderIcon className="postIcon postIconLiked" onClick={handleLikeClick} />
+                ) : (
+                  <FavoriteBorderIcon className="postIcon" onClick={handleLikeClick} />
+                )}
             <ChatBubbleOutlineIcon className="postIcon" />
             <TelegramIcon className="postIcon" />
           </div>
@@ -38,4 +96,4 @@ function post({ post }) {
   );
 }
 
-export default post
+export default Post
