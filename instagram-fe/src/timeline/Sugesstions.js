2b2ae7c72f3ade92f4db1c3de18b817755662d83
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-10 03:13:02 +0300

    added fetching all users feature

--- instagram-fe/src/timeline/Sugesstions.js
+++ instagram-fe/src/timeline/Sugesstions.js
@@ -1,24 +1,84 @@
 import { Avatar } from "@mui/material";
-import React from "react";
-import "./Sugesstions.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import './Sugesstions.css'
 
 function Suggestions() {
+  const [allUsers, setAllUsers] = useState([]);
+  const jwtToken = Cookies.get("jwt_token");
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []);
+
+  const fetchAllUsers = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/users", {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+      setAllUsers(response.data.users);
+    } catch (error) {
+      console.error("Error fetching all users:", error);
+    }
+  };
+
+  const handleFollow = async (userId) => {
+    try {
+      await axios.post(`http://127.0.0.1:8000/api/user/follow/${userId}`, {}, {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+      // Update the state to reflect the change
+      setAllUsers(allUsers.map(user => user.id === userId ? { ...user, is_followed: true } : user));
+    } catch (error) {
+      console.error("Error following user:", error);
+    }
+  };
+
+  const handleUnfollow = async (userId) => {
+    try {
+      await axios.post(`http://127.0.0.1:8000/api/user/unfollow/${userId}`, {}, {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+      // Update the state to reflect the change
+      setAllUsers(allUsers.map(user => user.id === userId ? { ...user, is_followed: false } : user));
+    } catch (error) {
+      console.error("Error unfollowing user:", error);
+    }
+  };
+
   return (
     <div className="suggestions">
       <div className="suggestions__title">Suggestions for you</div>
       <div className="suggestions__usernames">
-        <div className="suggestions__username">
-          <div className="username__left">
-            <span className="avatar">
-              <Avatar>R</Avatar>
-            </span>
-            <div className="username__info">
-              <span className="username">redian_</span>
-              <span className="relation">New to Instagram</span>
+        {allUsers.map(user => (
+          <div key={user.id} className="suggestions__username">
+            <div className="username__left">
+              <span className="avatar">
+                 <Avatar alt={user.name} src={user.image} />
+              </span>
+              <div className="username__info">
+                <span className="username">{user.username}</span>
+                <span className="relation">New to Instagram</span>
+              </div>
             </div>
+            {user.is_followed ? (
+              <button className="follow__button" onClick={() => handleUnfollow(user.id)}>
+                Unfollow
+              </button>
+            ) : (
+              <button className="follow__button" onClick={() => handleFollow(user.id)}>
+                Follow
+              </button>
+            )}
           </div>
-          <button className="follow__button">Follow</button>
-        </div>
+        ))}
       </div>
     </div>
   );
